ba5c4e191b54825254179d00148c5bf2b2850388
shiyuyullin <dayu19990519@gmail.com>
2020-07-28 11:20:30 -0400

    changes on order list & implement submit button

--- js/backstore.js
+++ js/backstore.js
@@ -19,12 +19,3 @@ function deleteRow(tableID,currentRow) {
     }
     //getValues();
 }
-
-function insertARow(tableID) {
-    var table = document.getElementById(tableID);
-    var row = table.insertRow(0);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    cell1.innerHTML = "NEW CELL1";
-    cell2.innerHTML = "NEW CELL2";
-  }

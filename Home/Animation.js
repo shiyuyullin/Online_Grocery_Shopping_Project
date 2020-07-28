862371cf63a488b7eb5a9574271779ffc8e809bc
Aboodkh1995 <67989407+Aboodkh1995@users.noreply.github.com>
2020-07-28 15:25:05 -0400

    Add files via upload

---
+++ Home/Animation.js
@@ -0,0 +1,26 @@
+
+var slideIndex = 0;
+
+
+function nextSlide() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("circle");
+  // go to next one by making background looks white
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  // change ciricl background
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" circleChColor", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " circleChColor";
+  setTimeout(nextSlide, 2500);
+}
+
+
+
+

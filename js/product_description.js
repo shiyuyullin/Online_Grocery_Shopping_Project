1f7edc424b3bc746eb08bdba77e47023e0710a92
xknoll56 <xavierknoll56@gmail.com>
2020-07-29 16:27:23 -0400

    moving js to folder and out of product descriptions

---
+++ js/product_description.js
@@ -0,0 +1,11 @@
+var acc = document.getElementsByClassName("accordion");
+
+acc[0].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+        panel.style.display = "none";
+    } else {
+        panel.style.display = "block";
+    }
+});

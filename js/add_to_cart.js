7d05d7b4b93abb62d8c14cacd9cfb46166b7a8d7
xknoll56 <xavierknoll56@gmail.com>
2020-08-16 09:06:24 -0400

    Merge issue

--- js/add_to_cart.js
+++ js/add_to_cart.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 var modalparent = document.getElementsByClassName("modal_multi");
 
@@ -73,46 +72,3 @@ window.onclick = function (event) {
 
 
 }
-=======
-      
-        var modalparent = document.getElementsByClassName("modal_multi");
-
-        var modal_btn_multi = document.getElementsByClassName("myBtn_multi");
-
-        var span_close_multi = document.getElementsByClassName("close_multi");
-
-        function setDataIndex() {
-
-            for (i = 0; i < modal_btn_multi.length; i++)
-            {
-                modal_btn_multi[i].setAttribute('data-index', i);
-                modalparent[i].setAttribute('data-index', i);
-                span_close_multi[i].setAttribute('data-index', i);
-            }
-        }
-        for (i = 0; i < modal_btn_multi.length; i++)
-        {
-            modal_btn_multi[i].onclick = function() {
-                var ElementIndex = this.getAttribute('data-index');
-                modalparent[ElementIndex].style.display = "block";
-            };
-            span_close_multi[i].onclick = function() {
-                var ElementIndex = this.getAttribute('data-index');
-                modalparent[ElementIndex].style.display = "none";
-            };
-
-        }
-
-        window.onload = function() {
-
-            setDataIndex();
-        };
-
-        window.onclick = function(event) {
-            if (event.target === modalparent[event.target.getAttribute('data-index')]) {
-                modalparent[event.target.getAttribute('data-index')].style.display = "none";
-            }
-
-           
-        }
->>>>>>> parent of f9f75af... Merge branch 'master' of https://github.com/shiyuyullin/soen287Project
